2587497e3a371c05bbb4a12a95aaf93d293edab3
matiastarasido <matiastarasido@gmail.com>
2021-06-30 21:53:29 -0300

    subo inicio

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.get('/', (req,res) =>{
+   res.sendFile(path.join(__dirname, './views/home.html'));
+});
+
+app.use(express.static(path.join(__dirname, './public')));
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Servidor corriendo en el puerto 3000");
+});
