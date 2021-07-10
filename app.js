9288d04574a5b28542ae090aefee7789699e058f
matiastarasido <matiastarasido@gmail.com>
2021-07-10 11:41:32 -0300

    enlaces de nuevo

--- app.js
+++ app.js
@@ -1,27 +1,14 @@
-const express = require('express');
+const express = require("express");
+const app = express();
 const path = require('path');
 
-const app = express();
-
-app.get('/', (req,res) =>{
-   res.sendFile(path.join(__dirname, './views/home.html'));
+app.listen(process.env.PORT || 3000, function(){ 
+    console.log("Servidor corriendo en el puerto 3000")
 });
 
-app.use(express.static(path.join(__dirname, './public')));
-
-
-
-app.get('/login', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/login.html'));
-  });
-app.get('/register', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/register.html'));
-  });
-
-
-
-
-
-app.listen(process.env.PORT || 3001, function () {
-    console.log("Servidor corriendo en el puerto 3001");
+ app.get("/", function(req, res){
+    res.sendFile(path.resolve(__dirname, "./views/home.html"))
 });
+
+app.use(express.static(path.resolve(__dirname, './public')));
+app.use(express.static(path.resolve(__dirname, './views')));
